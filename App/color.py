c30f5e54ba546f39ad140d97088ff1e5e0937c66
pythonbeater <pythonanon@gmail.com>
2023-05-31 14:56:32 +0100

    Game configuration

---
+++ App/color.py
@@ -0,0 +1,8 @@
+'''
+Responsible for game colors
+'''
+
+class Color: 
+    def __init__(self, light, dark) -> None:
+        self.light = light
+        self.dark = dark
---
+++ App/sounds.py
@@ -0,0 +1,14 @@
+'''
+Responsible for game sounds
+'''
+
+import pygame 
+
+class Sound: 
+
+    def __init__(self, path) -> None: 
+        self.path = path
+        self.sound = pygame.mixer.Sound(path)
+
+    def play(self): 
+        pygame.mixer.Sound.play(self.sound)
